7ff9695acd169df25ec1a41732b7fce6c54a28d2
kenokim <keno1413@gmail.com>
2022-05-21 10:53:22 +0900

    Chore : init

---
+++ src/main/java/keno/SnsChats/SnsChatsApplication.java
@@ -0,0 +1,13 @@
+package keno.SnsChats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsChatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsChatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/keno/SnsChats/SnsChatsApplicationTests.java
@@ -0,0 +1,13 @@
+package keno.SnsChats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsChatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
